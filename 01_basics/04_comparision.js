701fe8d75a4804f99807b49cb3507f7c765f67f1
SSM2011 <167677948+SSM2011@users.noreply.github.com>
2025-07-19 16:51:47 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2>=1)
+// console.log( 2 == 1)  //These are basic comparisions
+
+console.log("02" > 1)
+console.log("2" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)  //=> comparision convert null to a number, treating it as zero thats why we get the output true
+
+console.log(undefined >= 0) // => it gives false
+
+// === 
+
+console.log("2" === 2);
