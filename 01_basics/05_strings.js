7bc97df30a1313d1d423f52a0008009e856fe2b9
SSM2011 <167677948+SSM2011@users.noreply.github.com>
2025-07-19 17:39:13 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+const name = "Pranav"
+const repoCount = 50
+
+// console.log(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+
+const gameName = new String('subodhsm')
+
+console.log(gameName[1])
+console.log(gameName.__proto__)
+
