93d0a11e131b6c5db2ac88a07c3846ad49154e3a
SSM2011 <167677948+SSM2011@users.noreply.github.com>
2025-07-19 16:17:06 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+// let score = "33"
+//  33 is number but 33abc is NaN
+
+// let score = null  //0 
+
+// let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+let isLoggedIn = "subodh";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "subodh" => true
+
+let someNumber = "33"
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
