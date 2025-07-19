6057b7cf9a00adcf633f20deeeef4cd45dd9cded
SSM2011 <167677948+SSM2011@users.noreply.github.com>
2025-07-19 15:52:17 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3)  we are using nodejs, not browser
+
+// console.log(3 + 3); console.log("Subodh");  code is executable but bad practice in readability
+
+let name = "subodh"
+ let  age = 21
+let isLoggedIn = false;
+
+let state = null;
+
+// number  => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true or false
+//null => standalone value
+//undefine => 
+//symbol => unique
+
+
+//object => 
+
+console.log(typeof "subodh")  //string
+console.log(typeof null); //object
+console.log(typeof undefined) //undefined
