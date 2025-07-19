f6d37227ea0622c88a31a69484864a03a771561f
SSM2011 <167677948+SSM2011@users.noreply.github.com>
2025-07-19 16:37:03 +0000

    operations done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -26,3 +26,36 @@ let someNumber = "33"
 let stringNumber = String(someNumber)
 
 console.log(typeof stringNumber)
+
+
+
+// **************************************  Oprations *****************************************
+
+
+let value = 3
+let Negvalue = -value * 5;
+console.log(Negvalue)
+
+let str1 = "hello"
+let str2 = " subodh"
+
+let str3 = (str1+str2)
+
+console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + 2 + "2")
+// console.log("1" + 2 + 2);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+// num1 = num2 = num3 = 2+ 2   bad way to write code 
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
