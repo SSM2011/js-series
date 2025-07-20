f07abfd9dab60a97335d39786dc928a5cc1117a2
SSM2011 <167677948+SSM2011@users.noreply.github.com>
2025-07-20 10:16:33 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,65 @@
+// var c = 300
+
+let a = 300
+
+if (true) {
+    let a = 10
+    const b = 20
+    // var c = 30
+    console.log(a)
+
+    // function addnum(){
+
+    // }
+}
+
+// var c = 3000
+
+console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+function one(){
+    const username = "subodh"
+
+    function two(){
+        const website ="yt"
+        console.log(username)
+        // one()
+    }
+        // console.log(website)
+       
+        two()
+
+
+}
+
+one()
+
+
+if (true){
+    const username = "subodh";
+    if (username === "subodh"){
+        const website = " yt"
+        console.log(username + website)
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+
+// *************** interesing *****************
+
+
+console.log(addOne(5))
+function addOne(value){
+     return value + 1
+}
+
+
+const addTwo = function(value){
+    return value + 2
+}
+console.log(addTwo(5))
