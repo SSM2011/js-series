01d2ae6d9cbe7325ac1f435de133ccfec5af1409
SSM2011 <167677948+SSM2011@users.noreply.github.com>
2025-07-20 05:25:16 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,8 +6,40 @@ const repoCount = 50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
 
 
-const gameName = new String('subodhsm')
+const gameName = new String('subodh-sm')
 
 console.log(gameName[1])
 console.log(gameName.__proto__)
 
+console.log(gameName.length)
+console.log(gameName.toUpperCase()) // => original value does not change because it is in stack memory where we get the copy of primitive data types
+
+console.log(gameName)  
+console.log(gameName.charAt(2))
+
+console.log(gameName.indexOf("m"))
+console.log(gameName.toLowerCase());
+
+const newString = gameName.substring(0, 4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8, 4)
+
+console.log(anotherString)
+
+const newSTringOne = "   Subodh   "
+console.log(newSTringOne)
+console.log(newSTringOne.trim())   // => remove starting and end space
+
+const url = "https://subodh.com/subodh%20mule"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('subodh'))
+
+console.log(gameName.split('-'))
+
+
+const newvar = "new variable" 
+
+console.log(newvar.split(" "))
